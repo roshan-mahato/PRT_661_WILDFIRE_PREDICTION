f19fa51c32f98c4309c0f57a2ee6bbff54a970b7
Salin Panta <salinpant19@gmail.com>
2026-08-25 10:26:27 +1000

    add header component

--- frontend/main.py
+++ frontend/main.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write("Fire Prediction Application")
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.rand(1000,2) / [50,50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+st.map(map_data)
